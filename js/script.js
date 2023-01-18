38b73b4f9af79a280aab0e153cb50c8678eea94d
Maria Macarena Gomez Pujal <mmgomezpujal@gmail.com>
2023-01-18 17:21:45 -0300

    Se eliminó el alert duplicado del switch case, se colocó return vacio al final del lazo, se eliminó la variable incial total=0

--- js/script.js
+++ js/script.js
@@ -20,53 +20,52 @@ function calcularPrecio() {
 // BUCLE QUE ME PERMITE CONTINUAR MIENTRAS EL USUARIO NO SELECCIONE ESC
     while (idIngresado!="ESC") {
         let cantidad = prompt("Ingrese la cantidad deseada (en unidades): ");
-        let total = 0;
 // CONDICIONAL PARA VALIDAR LOS ID INGRESADOS Y CALCULAR PRECIO
         switch(idIngresado) {
             case 'id11':
-                alert("Seleccionaste la opción: emulsión en tambor x 200 kg")
-                total = precioId11 * cantidad;
-                return alert('El total de su compra es: ' + 'USD ' + total);
-            case 'id12':
-                alert("Seleccionaste la opción: emulsión en contenedor x 1000 kg");
+                // total = precioId11 * cantidad;
+                alert('Seleccionaste la opción: emulsión en tambor x 200 kg. El total de su compra es: ' + 'USD ' + precioId11*cantidad);
+                return; 
+                case 'id12':
                 total = precioId12 * cantidad;
-                return alert('El total de su compra es: ' + 'USD ' + total);
+                alert('Seleccionaste la opción: emulsión en contenedor x 1000 kg. El total de su compra es: ' + 'USD ' + total);
+                return;
             case 'id21':
-                alert("Seleccionaste la opción: dispersante en bidón/balde x 20 kg");
                 total = precioId21 * cantidad;
-                return alert('El total de su compra es: ' + 'USD ' + total);
+                alert('Seleccionaste la opción: dispersante en bidón/balde x 20 kg. El total de su compra es: ' + 'USD ' + total);
+                return;
             case 'id22':
-                alert("Seleccionaste la opción: dispersante en tambor x 200 kg");
                 total = precioId22 * cantidad;
-                return alert('El total de su compra es: ' + 'USD ' + total);
+                alert('Seleccionaste la opción: dispersante en tambor x 200 kg. El total de su compra es: ' + 'USD ' + total);
+                return;
             case 'id23':
-                alert("Seleccionaste la opción: dispersante en contenedor x 1000 kg");
                 total = precioId23 * cantidad;
-                return alert('El total de su compra es: ' + 'USD ' + total);
+                alert('Seleccionaste la opción: dispersante en contenedor x 1000 kg. El total de su compra es: ' + 'USD ' + total);
+                return;
             case 'id31':
-                alert("Seleccionaste la opción: antiespumante en bidón/balde x 20 kg");
                 total = precioId31 * cantidad;
-                return alert('El total de su compra es: ' + 'USD ' + total);
+                alert('Seleccionaste la opción: antiespumante en bidón/balde x 20 kg. El total de su compra es: ' + 'USD ' + total);
+                return;
             case 'id32':
-                alert("Seleccionaste la opción: antiespumante en tambor x 200 kg");
                 total = precioId32 * cantidad;
-                return alert('El total de su compra es: ' + 'USD ' + total);
+                alert('Seleccionaste la opción: antiespumante en tambor x 200 kg. El total de su compra es: ' + 'USD ' + total);
+                return;
             case 'id33':
-                alert("Seleccionaste la opción: antiespumante en contenedor x 1000 kg");
                 total = precioId33 * cantidad;
-                return alert('El total de su compra es: ' + 'USD ' + total);
+                alert('Seleccionaste la opción: antiespumante en contenedor x 1000 kg. El total de su compra es: ' + 'USD ' + total);
+                return;
             case 'id41':
-                alert("Seleccionaste la opción: biocida en bidón/balde x 20 kg");
                 total = precioId41 * cantidad;
-                return alert('El total de su compra es: ' + 'USD ' + total);
+                alert('Seleccionaste la opción: biocida en bidón/balde x 20 kg. El total de su compra es: ' + 'USD ' + total);
+                return;
             case 'id42':
-                alert("Seleccionaste la opción: biocida en tambor x 200 kg");
                 total = precioId42 * cantidad;
-                return alert('El total de su compra es: ' + 'USD ' + total);
+                alert('Seleccionaste la opción: biocida en tambor x 200 kg. El total de su compra es: ' + 'USD ' + total);
+                return;
             case 'id43':
-                alert("Seleccionaste la opción: biocida en contenedor x 1000 kg");
                 total = precioId43 * cantidad;
-                return alert('El total de su compra es: ' + 'USD ' + total);
+                alert('Seleccionaste la opción: biocida en contenedor x 1000 kg. El total de su compra es: ' + 'USD ' + total);
+                return;
             default:
                 idIngresado = prompt("Por favor, selecciona un id correcto:");
                 break;
